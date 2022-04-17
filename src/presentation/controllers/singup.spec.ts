959c9e137c1d22cfb191b9e01abb970248f496a9
lchirata <larissa.c.hirata@gmail.com>
2022-04-17 18:42:22 -0300

    test: ensure singupcontroller return 400 if no email is provided

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -12,5 +12,22 @@ describe('SingUp Controller', () => {
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    })
+})
+
+describe('SingUp Controller', () => {
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SingUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'))
     })
 })
