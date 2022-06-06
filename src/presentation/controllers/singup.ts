cb53fa5e14c09a1ecb42394cfc4540f612cd7dcb
lchirata <larissa.c.hirata@gmail.com>
2022-06-06 15:05:22 -0300

    refactor: simplify code

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,13 +4,11 @@ import { badRequest }  from '../helper/http-helper'
 
 export class SingUpController{
     handle(httpRequest: httpRequest): httpResponse{
-        if (!httpRequest.body.name) {
-            return badRequest( new MissingParamError('name')) 
-               
+        const requiredField = ['name', 'email']
+        for (const field of requiredField) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field))
+            }
         }
-        if (!httpRequest.body.email) {
-            return badRequest( new MissingParamError('email')) 
-        }
-        
     }
 }
