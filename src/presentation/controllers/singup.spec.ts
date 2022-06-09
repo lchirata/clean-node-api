901db72ce8781d1c3182030d9f400ffd11f8b5d3
lchirata <larissa.c.hirata@gmail.com>
2022-06-09 20:03:41 -0300

    test: esure SingUpController calls EmailValidator with correct email

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,13 +1,31 @@
 import { SingUpController } from './singup'
 import { MissingParamError } from '../error/missing-params-error'
+import { InvalidParamError } from '../error/invalid-params-error'
+import { EmailValidator } from '../protocols/email-validator'
 
-const makeSut = ():SingUpController => {
-    return new SingUpController()
+interface SutTypes {
+    sut: SingUpController,
+    emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+    class EmailValidatorStub implements EmailValidator { //stub é um tipo de teste.
+        isValid(email: string): boolean {
+            return true
+        }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SingUpController(emailValidatorStub)
+    return {
+        emailValidatorStub,
+        sut
+    }
 }
 
 describe('SingUp Controller', () => {
+
     test('Should return 400 if no name is provided', () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const httpRequest = {
             body: {
                 email: 'any_email@mail.com',
@@ -18,10 +36,9 @@ describe('SingUp Controller', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('name'))
-    }),
-            
+    }),     
     test('Should return 400 if no email is provided', () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const httpRequest = {
             body: {
                 name: 'any_name',
@@ -33,9 +50,8 @@ describe('SingUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
-
     test('Should return 400 if no email is provided', () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const httpRequest = {
             body: {
                 name: 'any_name',
@@ -47,7 +63,49 @@ describe('SingUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
-
-    
+    test('Should return 400 if invalid email is provided', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false) // utilizando jest para mocar retorno como false
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'invalid_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    })
+    test('Should call email validator with valid email', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false) // utilizando jest para mocar retorno como false
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'invalid_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    })
+    test('Should call email validator with correct email', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            }
+        }
+        sut.handle(httpRequest)
+        expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+    })
 })
 
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,14 +1,24 @@
 import { httpResponse, httpRequest } from '../protocols/http'
 import { MissingParamError } from '../error/missing-params-error'
-import { badRequest }  from '../helper/http-helper'
+import { badRequest } from '../helper/http-helper'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../error/invalid-params-error'
 
 export class SingUpController{
+    private readonly emailValidator: EmailValidator
+    constructor(emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator   
+    }
     handle(httpRequest: httpRequest): httpResponse{
-        const requiredField = ['name', 'email', 'password']
+        const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
         for (const field of requiredField) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
             }
+        }
+        const isValid = this.emailValidator.isValid(httpRequest.body.email)
+        if (!isValid) {
+            return badRequest(new InvalidParamError('email'))
         }
     }
 }
