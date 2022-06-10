dfc03a1be5532a0eaf0fc2d0107d50eb02dc5cd6
lchirata <larissa.c.hirata@gmail.com>
2022-06-10 19:17:14 -0300

    feat: ensure SingUpController returns 200 if valid data is provided

--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -26,11 +26,15 @@ export class SingUpController{
             if (!isValid) {
                 return badRequest(new InvalidParamError('email'))
             }
-        this.addAccount.add({
+        const account = this.addAccount.add({
             name,
             email,
             password
-            }) 
+        }) 
+            return {
+                statusCode: 200,
+                body: account
+        }
         } catch (error) {
             return serverError()    
        }
